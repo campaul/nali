24f8bb01f74dbd88dc774ed709f06e35d6a94e52
Cameron Paul <cpaul37@gmail.com>
2012-10-21 01:34:06 -0400

    Added rudimentary support for source files

--- __main__.py
+++ __main__.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 
 import readline
+import sys
 import interpreter
 
-interpreter.repl()
+if len(sys.argv) > 1:
+    i = interpreter.Interpreter()
+    
+    for line in open(sys.argv[1], 'r'):
+        i._eval(line)
+else:
+    interpreter.repl()
