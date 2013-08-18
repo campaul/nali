ef2778a85fb3e0c0a2599f03f6a56cc805ea581b
Cameron Paul <cpaul37@gmail.com>
2013-08-18 13:53:16 -0700

    Added tests for function namespaces

--- tests/namespace_test.py
+++ tests/namespace_test.py
@@ -12,3 +12,12 @@ class NamespaceTestCase(InterpreterTestCase):
     def test_global_scope(self):
         self.interpreter.eval('def :foo 42')
         T.assert_equal(self.interpreter.eval('[foo]'), 42)
+
+    def test_local_scope(self):
+        self.interpreter.eval('def :inc [|x| x + 1]')
+        T.assert_equal(self.interpreter.eval('inc 1'), 2)
+
+    def test_closure(self):
+        self.interpreter.eval('def :plusn [|x| [|y| x + 1]]')
+        self.interpreter.eval('def :inc (plusn 1)')
+        T.assert_equal(self.interpreter.eval('inc 1'), 2)
