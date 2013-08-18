8ed02c3b64ab0ec1458567ad0685cc506cbfd8ba
Cameron Paul <cpaul37@gmail.com>
2013-08-18 13:17:36 -0700

    Added basic math tests

---
+++ tests/math_test.py
@@ -0,0 +1,21 @@
+import testify as T
+
+from tests.nali_interpreter_test import InterpreterTestCase
+
+
+class MathTestCase(InterpreterTestCase):
+
+    def test_addition(self):
+        T.assert_equal(self.interpreter.eval('2 + 3'), 5)
+
+    def test_subtraction(self):
+        T.assert_equal(self.interpreter.eval('5 - 3'), 2)
+
+    def test_multiplication(self):
+        T.assert_equal(self.interpreter.eval('2 * 3'), 6)
+
+    def test_division(self):
+        T.assert_equal(self.interpreter.eval('8 / 2'), 4)
+
+    def test_modulo(self):
+        T.assert_equal(self.interpreter.eval('5 % 3'), 2)
